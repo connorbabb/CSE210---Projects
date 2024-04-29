0f264b1769549fa2f29cb5b06a1c1ce936e3951c
connorbabb <145483698+connorbabb@users.noreply.github.com>
2024-04-29 13:35:45 -0600

    finished prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        int sum = 0;
+        float average = 0;
+        int max = 0;
+        int small = 0;
+        foreach (int num in numbers)
+        {
+            sum = sum + num;
+            if (num > max)
+            {
+                max = num;
+            }
+            if (num < max && num > 0)
+            {
+                small = num;
+            }
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        float sum1 = sum;
+        float count = numbers.Count;
+        average = sum1 / count;
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The Largest Number is: {max}");
+        Console.WriteLine($"The Smallest Positive Number is: {small}");
     }
 }
--- sandbox/Pickles/Porgam.cs
+++ sandbox/Pickles/Porgam.cs
@@ -1,1 +1,7 @@
 Console.WriteLine("Hello World!");
+// Use to slashes to comment
+// String userInput:
+// Console.Write('Prompt');
+// userInput = Console.ReadLine();
+// int i;
+// bool b;
